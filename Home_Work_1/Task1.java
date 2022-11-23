abb369cd5826f88f6d884b0bf93ffd2307ead2ad
Rumyanceva <nj89.89@mail.ru>
2022-11-23 12:58:55 +0300

    Add Task1 in HomeWork1

--- Home_Work_1/Task1.java
+++ Home_Work_1/Task1.java
@@ -1,5 +1,27 @@
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+
+
 package Home_Work_1;
 
+import java.util.Scanner;
+
 public class Task1 {
-    
+   public static void main(String[] args) {
+
+    Scanner iScanner = new Scanner(System.in);
+    System.out.println("Input number: ");
+    int number = iScanner.nextInt();
+
+    int sum = 0;
+    int factorial = 1;
+
+    for(int i = 1; i <= number; i++) {
+        sum = sum + i;
+        factorial = factorial * i;
+    }
+
+    System.out.printf(("Треугольное число равно: %d\n"), sum);
+    System.out.printf(("Факториал равен: %d"), factorial);
+   } 
 }
